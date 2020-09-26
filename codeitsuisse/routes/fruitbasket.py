7e99dbf52c16bd602e728d0bcb5cb66bb2229799
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-09-26 17:46:05 +0800

    Parsing data as string instead of json

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -11,9 +11,10 @@ logger = logging.getLogger(__name__)
 def evaluateFruitBasket():
     data = request.get_data();
     logging.info("data sent for evaluation {}".format(data))
-    noBanana = data.get("maBanana");
-    noMelon = data.get("maWatermelon")
-    noApple = data.get("maApple")
+    splitString = data.split(",")
+    noBanana = splitString[0]
+    noMelon = splitString[1]
+    noApple = splitString[2]
     estimate = str(noApple*10+noBanana*10+noMelon*10)
     result = estimate
     logging.info("My result :{}".format(result))
