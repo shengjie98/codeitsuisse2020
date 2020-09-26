83d769701025528120ddeafdc0b2321ebce6a1c2
zhaojie-kok <kok.zhaojie@gmail.com>
2020-09-26 21:12:27 +0800

    contact

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -8,12 +8,7 @@ import codeitsuisse.routes.social
 import codeitsuisse.routes.portfolio
 import codeitsuisse.routes.gene
 import codeitsuisse.routes.clean_floor
-<<<<<<< HEAD
-import codeitsuisse.routes.inventory
-=======
+import codeitsuisse.routes.contact
 import codeitsuisse.routes.baby
 import codeitsuisse.routes.fruitbasket
 import codeitsuisse.routes.cluster
-
-
->>>>>>> cfa7aad7d44e1403b6c0626a598d42e7000afc1c
