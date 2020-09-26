ea1cb3ab7abc756613e9890437ea6bfa920cd303
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2020-09-26 12:09:34 +0800

    add gene

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,6 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
 import codeitsuisse.routes.saladspree
 import codeitsuisse.routes.revisit
 import codeitsuisse.routes.encrypt
 import codeitsuisse.routes.square
-from flask import Flask
-app = Flask(__name__)
+import codeitsuisse.routes.gene
+
