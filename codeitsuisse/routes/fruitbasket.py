a58784fd235c51e1f5c850ea8b0c9fff148769cd
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-09-26 16:58:53 +0800

    trying to get a response from fruitbasket

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -7,7 +7,7 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-@app.route('/square', methods=['POST'])
+@app.route('/fruitbasket', methods=['POST'])
 def evaluateFruitBasket():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
