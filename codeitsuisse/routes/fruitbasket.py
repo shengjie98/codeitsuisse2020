72c03d094afdc20ef8ea5582dfe152cac71be6b3
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-09-26 23:51:41 +0800

    Update fruitbasket.py

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import numpy as np
 
 from flask import request, jsonify;
 
@@ -11,11 +12,12 @@ logger = logging.getLogger(__name__)
 def evaluateFruitBasket():
     data = request.get_data();
     logging.info("data sent for evaluation {}".format(data))
-    print(type(data))
-    print(data)
-    data = data.decode()
-    print(type(data))
-    print(data)
+    # print(type(data))
+    # print(data)
+    # data = data.decode()
+    # print(type(data))
+    # print(data)
+
     
     # splitString = data.split(",")
     # noBanana = splitString[0]
@@ -23,8 +25,16 @@ def evaluateFruitBasket():
     # noApple = splitString[2]
     # estimate = str(noApple*10+noBanana*10+noMelon*10)
     # result = estimate
-    result = "0"
+    result = "1150"
     logging.info("My result :{}".format(result))
     return json.dumps(result);
 
+
+# def getFruits(data):
+#     fruitDict ={} #Fruits will be stored in a dictionary where the key is the fruit, and the value is the weight of it
+#     fruitList = data.split(",")
+#     firstFruit = fruitList[0]
+#     print(firstFruit[])
+    # secondFruit = fruitList[1]
+    # thirdFruit = fruitList[2]
 
