402ecc3312139c0396e826d2f5304171fa238a67
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2020-09-27 11:18:11 +0800

    Update tick.py

--- codeitsuisse/routes/tick.py
+++ codeitsuisse/routes/tick.py
@@ -1,7 +1,7 @@
 import logging
 import json
 from io import StringIO
-from pandas import pd
+import pandas as pd
 
 from flask import request, jsonify;
 
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 @app.route('/pre-tick', methods=['POST'])
 def evaluate_tick():
     data = request.get_data()
+    data = str(data, "utf-8")
     logging.info("data sent for evaluation {}".format(data))
     result = get_tick(data)
     logging.info("My result :{}".format(result))
@@ -22,6 +23,8 @@ def get_tick(csvStr):
     df = pd.read_csv(data)
     T = len(df) -1
     # df = pd.DataFrame([x.split(';') for x in csvStr.split('\n')])
-    cT = df['Close'].expanding(2).mean()
-    zT = df['Close'].expanding(2).std()
-    return (cT + zT).get(T)
+    cT = df["Close"].expanding(1).mean()
+    zT = df["Close"].expanding(1).std()
+    ans = (cT + zT).get(T)
+    logging.info("df: ".format(str(T)))
+    return ans
